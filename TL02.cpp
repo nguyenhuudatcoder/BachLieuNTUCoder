5b65d500e03bb672b2c69522b440d2e08ff30871
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-02-16 16:54:50 +0700

    Accepted

--- TL02.cpp
+++ TL02.cpp
@@ -5,16 +5,8 @@
 using namespace std;
 const ll inf=LLONG_MAX;
 const ll mod=1e9+7;
-int n;
-int a[1000005];
-void idk()
-{
-    sort(a,a+n);
-    int dem=0;
-    for(int i=0;i<n;i++)
-        if(a[i]>=dem)dem++;
-    cout<<dem;
-}
+int n,s,a[100005];
+int d;
 kien()
 {
     // if(fopen(".inp","r"))
@@ -25,6 +17,8 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin>>n;
-    for(int i=0;i<n;i++)cin>>a[i];idk();
+    cin>>n>>s;
+   for(int i=0;i<n;i++)cin>>a[i];
+   sort(a,a+n);
+    for(int i=0;i<n;i++)if(s>=a[i]){s+=a[i];d++;}cout<<d;
 }
