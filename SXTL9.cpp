da495425b309fad1c4a25a326dfd3299263f37b7
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-02-19 08:48:12 +0700

    Accepted

---
+++ SXTL9.cpp
@@ -0,0 +1,30 @@
+///Made by Nguyễn Hữu Đạt
+#include<bits/stdc++.h>
+#define ll long long
+#define kien main
+using namespace std;
+const ll inf=LLONG_MAX;
+const ll mod=1e9+7;
+int n;
+int a[100005];
+int d;
+ll ans;
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>n;
+    for(int i=0;i<n;i++)cin>>a[i];sort(a,a+n,greater<int>());
+    for(int i=0;i<n;i++)
+    {
+        ans+=max(0,a[i]-d);
+        d++;
+    }cout<<ans;
+}
+    
